6605eb264cdf7318498c4b7143749477f243a1d3
M Krupananda <m.krupananda@accolite.com>
2023-08-24 00:31:23 +0530

    implemented examples for variables

---
+++ Python/prc.py
@@ -0,0 +1,32 @@
+# variables
+
+# a="sai"
+# print(a)
+
+# a,b,c="sai","krupa","nanda"
+# print(a,b,c)
+
+# a=b="msk"
+# print(a,b)
+
+# list=[1,2,3]
+# a,b,c=list
+# print(a,b,c)
+
+
+# '''global & local variables'''
+# x="global"
+
+# def func():
+#     global x
+#     x = "local"
+#     print(x)
+
+# func()
+# print(x)
+
+
+# data types
+
+x=5
+print(type(x))
